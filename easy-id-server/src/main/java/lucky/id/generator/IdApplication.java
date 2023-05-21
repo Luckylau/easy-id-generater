c98c5a675bc73df4c7101524d4d22a5fd5517f00
luckylau <laujunbupt0203@163.com>
2023-05-21 09:45:24 +0800

    feat(id-generator): 一些基本提交

---
+++ easy-id-server/src/main/java/lucky/id/generator/IdApplication.java
@@ -0,0 +1,17 @@
+package lucky.id.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author luckylau
+ * @Date 2023/5/21
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+public class IdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdApplication.class);
+    }
+}
---
+++ easy-id-server/src/test/java/lucky/id/generator/IdApplicationTest.java
@@ -0,0 +1,7 @@
+package lucky.id.generator;
+
+/**
+ * Unit test for simple App.
+ */
+public class IdApplicationTest {
+}
